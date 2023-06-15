8bd85214538bd89f419760f4da091ee63bc38357
Vitya Smirnov <335158@niuitmo.ru>
2023-06-15 11:38:32 +0000

    #6 [jetson-service] Добавил вывод данных об обнаруженных объектах в консоль

--- computer-vision-on-single-board-computers/prototype/jetson-service/src/main.py
+++ computer-vision-on-single-board-computers/prototype/jetson-service/src/main.py
@@ -1,12 +1,29 @@
+from typing import List
 from service import Recognizer
 from jetson.video import ImageInput, ImageOutput
 from jetson.inference import Detector
+from model import Detection
+
+
+def output_detected(objects: List[Detection]):
+    if len(object) == 0: return
+    print('[JS] --- Detected Objects --- ')
+    for object in objects:
+        print(f'[JS] Detection with confidence {object.confidence:.2f}')
+        print(f'[JS]   left:   {object.box.left:.2f}')
+        print(f'[JS]   right:  {object.box.right:.2f}')
+        print(f'[JS]   top:    {object.box.top:.2f}')
+        print(f'[JS]   bottom: {object.box.bottom:.2f}')
+        print()
+    print('[JS] --- ---------------- --- ')
+    print()
+    print()
 
 
 if __name__ == '__main__':
     Recognizer(
         model = Detector(Detector.Parameters(
-            name = "peoplenet",
+            name = 'peoplenet',
             threshold = 0.3,
             tracking = Detector.Parameters.Tracking(
                 min_frames = 3,
@@ -14,6 +31,10 @@ if __name__ == '__main__':
                 overlap_threshold = 0.5,
             )
         )),
-        input = ImageInput("csi://0"),
-        output = ImageOutput("rtp://192.168.1.40:1234")
+        video_input = ImageInput('csi://0'),
+        video_output = ImageOutput('rtp://192.168.1.100:1234'),
+        objects_output = output_detected,
+        config = Recognizer.Config(
+            detection_period = 15,
+        ),
     ).run()
--- computer-vision-on-single-board-computers/prototype/jetson-service/src/service/recognizer.py
+++ computer-vision-on-single-board-computers/prototype/jetson-service/src/service/recognizer.py
@@ -1,14 +1,25 @@
+from typing import NamedTuple, Callable
 from jetson.inference import Detector
 from jetson.video import ImageInput, ImageOutput
+from model import Detection
 
 
-class Recognizer:
-    def __init__(self, model: Detector, input: ImageInput, output: ImageOutput):
-        self.model  = model
-        self.input  = input
-        self.output = output
+class Recognizer(NamedTuple):
+    class Config(NamedTuple):
+        detection_period: int
+
+    model: Detector
+    video_input: ImageInput
+    video_output: ImageOutput
+    objects_output: Callable[[Detection], None]
+    config: Config
 
     def run(self):
-        for image in self.input:
-            _ = self.model.objects_on(image)
-            self.output.consume(image)
+        i, period = 0, self.config.detection_period
+        for image in self.video_input:
+            if i == 0:
+                objects = self.model.objects_on(image)
+                objects = list(map(Detection.from_jetson, objects))
+                self.objects_output((objects))
+            self.video_output.consume(image)
+            i = (i + 1) % period
